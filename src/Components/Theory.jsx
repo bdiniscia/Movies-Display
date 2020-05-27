c5094ffef247af8c98f91825f04e1ae56497563b
bdiniscia <bdiniscia@gmail.com>
2020-05-27 00:09:35 -0400

    Making the Theory part

--- src/Components/Theory.jsx
+++ src/Components/Theory.jsx
@@ -5,7 +5,9 @@ const Theory = () => {
     return (
         <div className='theory'>
             <h5>¿Explique la diferencia entre inputs controlados y no controlados?</h5>
+            <p>Los formularios controlados son manejados por el propio React, y los no controlados son manejados por el DOM. Siempre es preferible usar los manejados por React, aunque a veces amerita usar no controlados</p>
             <h5>¿Cómo prevenir que los components vuelvan a ser re-renderizados?</h5>
+            <p>Con renderizados condicionales, </p>
             <h5>¿Qué son los HOC? Agregar ejemplo.</h5>
             <h5>¿Qué es React Redux?</h5>
             <h5>¿Para qué sirven las ‘Arrow function’ en react?</h5>            
