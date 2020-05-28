72e774da30c5e72fc81e28700a0c631222af754d
bdiniscia <bdiniscia@gmail.com>
2020-05-27 21:21:51 -0400

    Updates

---
+++ precache-manifest.ff0c7e726c1f44e7ffd64d1da1969042.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8901b881528537fcc8191750a555c77b",
+    "url": "/Movies-Display/index.html"
+  },
+  {
+    "revision": "0f779588708915d77fee",
+    "url": "/Movies-Display/static/css/main.4467de87.chunk.css"
+  },
+  {
+    "revision": "67200caf15cdd3d212e7",
+    "url": "/Movies-Display/static/js/2.c3fb45b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Movies-Display/static/js/2.c3fb45b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f779588708915d77fee",
+    "url": "/Movies-Display/static/js/main.f44369df.chunk.js"
+  },
+  {
+    "revision": "59b347891ed5e453c7c3",
+    "url": "/Movies-Display/static/js/runtime-main.cf2366b3.js"
+  },
+  {
+    "revision": "9d28016c38f8a4d6ff16406a04a99e74",
+    "url": "/Movies-Display/static/media/1-compon.9d28016c.png"
+  },
+  {
+    "revision": "421693c74ea60afd0601369fc9231621",
+    "url": "/Movies-Display/static/media/2-compon.421693c7.png"
+  },
+  {
+    "revision": "6c7ff4727c27e794bec57ed429d6c7ab",
+    "url": "/Movies-Display/static/media/arrow.6c7ff472.svg"
+  },
+  {
+    "revision": "f93ec02ce1cd6d4d156aca05df320cf1",
+    "url": "/Movies-Display/static/media/nunchee-test.f93ec02c.png"
+  }
+]);
